0595a0bb355277e34a55129270d1befa331bf364
Suppiluliuma <jesse.nevalainen@myy.haaga-helia.fi>
2023-09-15 20:22:34 +0300

    valmis

--- App.js
+++ App.js
@@ -1,14 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TextInput, Button, FlatList, Alert, Image } from 'react-native';
 
 export default function App() {
-  const [keyword, setKeyWord] = useState("www.themealdb.com/api/json/v1/1/filter.php?i="+{keyword});
-  const [api,setApi] = useState("");
+  
+  const [keyword, setKeyword] = useState("");
+  const [repository, setRepository] = useState([]);
+  const getRepositories = () => {
+    fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=' + keyword)
+      .then(response => {
+        //console.log(response)
+        return response.json()
+      })
+      .then(data => setRepository(data.meals))
+      .catch(error => {
+        Alert.alert('Error', error.message);
+      });
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <TextInput
+        keyboardType="default"
+        style={styles.input}
+        placeholder='Keyword'
+        value={keyword}
+        onChangeText={text => setKeyword(text)}
+      />
+      <Button title="Find" onPress={getRepositories} />
+
+      <FlatList
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) =>
+          <View style={styles.item}>
+            <Text style={{ fontSize: 18, fontWeight: "bold" }}>{item.strMeal}</Text>
+            <Image style = {{width:200,height:200}} source={{
+              uri: item.strMealThumb
+            }} />
+
+          </View>}
+        data={repository}
+      />
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -19,5 +53,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    alignContent: 'center',
+    marginTop: 200
+  },
+  input: {
+    width: 200,
+    height: 40,
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 10,
+    backgroundColor: 'lightblue',
+  },
+  item: {
+    borderBottomWidth: 1,
+    borderBottomColor: 'lightgray', 
+    padding: 16,
   },
 });
