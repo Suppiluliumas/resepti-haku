656d439be938ec9b2d8fabc0dd8f9da579c53759
Suppiluliuma <jesse.nevalainen@myy.haaga-helia.fi>
2023-09-15 13:54:40 +0300

    kesken

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [keyword, setKeyWord] = useState("www.themealdb.com/api/json/v1/1/filter.php?i="+{keyword});
+  const [api,setApi] = useState("");
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
